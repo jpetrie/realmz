05087e22eea40fefa13e561862bef73ddf9669cc
Dan Applegate <applegatedt@gmail.com>
2024-09-22 21:36:59 -0400

    Fix formatting of function declarations

--- src/MenuManager.hpp
+++ src/MenuManager.hpp
@@ -16,13 +16,14 @@ struct Menu {
     Item() = default;
     ~Item() = default;
 
-    Item(ResourceDASM::ResourceFile::DecodedMenu::Item& item) : name{item.name},
-                                                                icon_number{item.icon_number},
-                                                                key_equivalent{item.key_equivalent},
-                                                                mark_character{item.mark_character},
-                                                                style_flags{item.style_flags},
-                                                                enabled{item.enabled},
-                                                                checked{false} {}
+    Item(ResourceDASM::ResourceFile::DecodedMenu::Item& item)
+        : name{item.name},
+          icon_number{item.icon_number},
+          key_equivalent{item.key_equivalent},
+          mark_character{item.mark_character},
+          style_flags{item.style_flags},
+          enabled{item.enabled},
+          checked{false} {}
   };
 
   int16_t menu_id;
@@ -34,10 +35,11 @@ struct Menu {
   Menu() = default;
   ~Menu() = default;
 
-  Menu(ResourceDASM::ResourceFile::DecodedMenu& decoded_menu) : menu_id{decoded_menu.menu_id},
-                                                                proc_id{decoded_menu.proc_id},
-                                                                title(decoded_menu.title),
-                                                                enabled(decoded_menu.enabled) {
+  Menu(ResourceDASM::ResourceFile::DecodedMenu& decoded_menu)
+      : menu_id{decoded_menu.menu_id},
+        proc_id{decoded_menu.proc_id},
+        title(decoded_menu.title),
+        enabled(decoded_menu.enabled) {
     for (auto& item : decoded_menu.items) {
       // Convert DecodedMenu::Item list to Menu::Item list
       this->items.emplace_back(item);
